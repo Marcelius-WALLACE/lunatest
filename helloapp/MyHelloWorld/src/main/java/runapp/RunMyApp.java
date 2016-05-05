71203a4e85501fcb79bde3332c2045d6d239273a
Marcelius-WALLACE <Marcelius-WALLACE@users.noreply.github.com>
2016-05-05 17:28:45 +0200

    Premier chargement

---
+++ helloapp/MyHelloWorld/src/main/java/runapp/RunMyApp.java
@@ -0,0 +1,15 @@
+package runapp;
+
+public class RunMyApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println(doHello ());
+
+	}
+	
+	private static String doHello () {
+		return "Magic Hello World !" ;
+	}
+
+}
